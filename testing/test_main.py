4451d292849be13e3919687a2c4ab83bccfecb48
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-14 12:43:26 +0100

    Fixed tests

--- testing/test_main.py
+++ testing/test_main.py
@@ -12,7 +12,7 @@ from AutoFeedback.randomclass import randomvar
 import unittest
 from main import *
 
-x = np.linspace(1,100,100)
+x = [0,1]
 var = randomvar( [prob,1-prob], variance=[prob*(1-prob),prob*(1-prob)], vmin=[0,0], vmax=[1,1], isinteger=[True,True] ) 
 line1=line( x, var )
 
@@ -20,4 +20,4 @@ axislabels=["Outcome", "Probability"]
 
 class UnitTests(unittest.TestCase) :
     def test_plot(self) :
-        assert(check_plot([line1],explabels=axislabels,is_bar_chart=True,explegend=False,output=True))
+        assert(check_plot([],exppatch=line1,explabels=axislabels,explegend=False,output=True))
